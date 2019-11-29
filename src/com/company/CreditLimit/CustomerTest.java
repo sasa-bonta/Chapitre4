8d5001a06f2d384f33009045b268ef57648cb136
sasa-bonta <bonta2000@mail.ru>
2019-11-29 09:44:22 +0200

    Nr 18 done

--- src/com/company/CreditLimit/CustomerTest.java
+++ src/com/company/CreditLimit/CustomerTest.java
@@ -1,18 +1,43 @@
 package com.company.CreditLimit;
 
+import java.util.Scanner;
+
 public class CustomerTest {
 
     public static void main(String[] args) {
-        Customer customer1 = new Customer(1, 1000, 500, 2000, 100);
-        newBalance(customer1);
-        Customer customer2 = new Customer(2, 1000, 500, 750, 200);
-        newBalance(customer2);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Enter account number, balance, debit, credit, limit / or -1 to quit : ");
+
+        int accountNumber = input.nextInt();
+        int balanceBegin = input.nextInt();
+        int debit = input.nextInt();
+        int credit = input.nextInt();
+        int allowedCredit = input.nextInt();
+
+        Customer customer = new Customer(accountNumber, balanceBegin, debit, credit, allowedCredit);
+
+
+        while (accountNumber != -1) {
+            newBalance(customer);
+
+            System.out.printf("Enter account number, balance, debit, credit, limit / or -1 to quit : ");
+
+            accountNumber = input.nextInt();
+            if (accountNumber == 0)
+                break;
+            balanceBegin = input.nextInt();
+            debit = input.nextInt();
+            credit = input.nextInt();
+            allowedCredit = input.nextInt();
+            customer = new Customer(accountNumber, balanceBegin, debit, credit, allowedCredit);
+        }
     }
 
     public static void newBalance(Customer customer){
-        //System.out.printf("%d balance is %d", accountNumber, balanceBegin + debit -credit);
         customer.setBalanceBegin(customer.getBalanceBegin() + customer.getDebit() - customer.getCredit());
-        if (customer.getBalanceBegin() > customer.getAllowedCredit())
+        if (customer.getBalanceBegin() >= -1 * customer.getAllowedCredit())
             System.out.printf("Account %d balance is %d%n", customer.getAccountNumber(), customer.getBalanceBegin());
         else
             System.out.printf("Account %d Credit limit exceeded%n", customer.getAccountNumber());
